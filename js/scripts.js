2b5560fe73d9ad0fbf7a3d72335c4f012ac4af3e
Alesia-Bahdanava <alesia.job@gmail.com>
2025-04-08 18:12:12 -0600

    add scripts file

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert ("Hey, you! Welcome to my page");
+    console.log ("Hello World! This is Alesia Bahdanava");
+
+    document.getElementById
+});
